a3c23df071f32da1d7bda1e77ab6379ccb4edf94
tarik <ballalyze@gmail.com>
2025-10-08 15:58:24 +0200

    Initialize script and define global variables

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Wait until page loads
+document.addEventListener("DOMContentLoaded", function () {
+  // Global variables withen the main function after loading
+    const usernameInput = document.getElementById("username");
+    const startBtn = document.getElementById("start");
+    const nextBtn = document.getElementById("next-btn");
+    const resetBtn = document.getElementById("reset-btn")
+    const playAgainBtn = document.getElementById("restart-btn");
+    let userName = '';
+
+
+
+
+
+
+
+
+
+
+
+
+});
