92ba3a5fcba7466f512f0a216116760d263d9160
tarik <ballalyze@gmail.com>
2025-10-08 16:01:44 +0200

    Add start button logic and level selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,27 @@ document.addEventListener("DOMContentLoaded", function () {
     const playAgainBtn = document.getElementById("restart-btn");
     let userName = '';
 
+    // listen to statr btn click !
+    startBtn.addEventListener("click", function () {
+    const username = usernameInput.value.trim();
+    userName = username;
+    // making sure that the field is not empty or have only spaces !
+    if (username === "") {
+        alert("Please enter your name!");
+        return;
+    }
+    // get the current level from the radio input 
+    const selectedLevel = document.querySelector('input[name="level"]:checked').value;
+    
+    // hide the intro section
+    document.getElementById("intro-section").classList.add("hidden");
+    // display the quiz section by removing the "hidden" class
+    document.getElementById("quiz-section").classList.remove("hidden");
+
+    // passes the selected level to runGame function
+    runGame(selectedLevel);
+    });
+
 
 
 
